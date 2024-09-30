b43b397e02f7e526543c4bc8fabdb6efb7c727c3
Igor Benites Moura <88231773+Darkyie@users.noreply.github.com>
2024-09-30 12:15:20 -0300

    Create teste.cs

---
+++ Back-end/teste.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BackTesteProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub Actions!");
+            // Simulando um erro para testes
+            throw new Exception("Erro de teste!");
+        }
+    }
+}
