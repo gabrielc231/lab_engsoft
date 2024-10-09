9261c11afb39f99cf732aea9e040c1b135527ac8
Igor Benites Moura <88231773+Darkyie@users.noreply.github.com>
2024-10-08 21:48:46 -0300

    Update TesteEmissao.cs

--- Back-end/Tests/TesteEmissao.cs
+++ Back-end/Tests/TesteEmissao.cs
@@ -1,26 +1,30 @@
-using Xunit;
+using Xunit; 
+using EcoImpacto.Application.UseCases.Calculator;
+using EcoImpacto.Communication.Requests;
+using EcoImpacto.Communication.Responses;
 
-public class EmissaoCarbonoTests
+public class RegisterCalculatorDataUseCaseTests
 {
     [Fact] 
-    public void CalcularEmissaoCarbono_DeveRetornarValorCorreto()
+    public void Execute_DeveRetornarEmissaoCorreta()
     {
-       
-        var dados = new DadosImpacto
+        
+        var useCase = new RegisterCalculatorDataUseCase();
+        
+        var request = new RequestCalculatorResultJson
         {
             ConsumoEnergiaKWh = 100,   
-            ConsumoGasM3 = 50,        
+            ConsumoGasM3 = 50,         
             ConsumoCombustivelLitros = 20,  
-            TransportePublicoKm = 10   
+            TransportePublicoKm = 10,  
+            PraticasReciclagem = 5   
         };
 
-        var calculadora = new CalculadoraCarbono();
+      
+        var response = useCase.Execute(request);
         
-   
-        double resultado = calculadora.CalcularEmissaoCarbono(dados);
-        
-        
-        double resultadoEsperado = (100 * 0.233) + (50 * 2.1) + (20 * 2.3) + (10 * 0.1);
-        Assert.Equal(resultadoEsperado, resultado, precision: 2); 
+       
+        double expectedEmission = (100 * 0.223) + (50 * 2.1) + (20 * 2.3) + (10 * 0.1);
+        Assert.Equal(expectedEmission, response.result, precision: 2); // Verificando o valor da emissão
     }
 }
