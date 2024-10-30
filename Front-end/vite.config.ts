e79f8666e2f65f2ed528fdb9e331ded6ba883692
gabrielc231 <142500547+gabrielc231@users.noreply.github.com>
2024-10-30 04:32:30 -0300

    Update vite.config.ts

--- Front-end/vite.config.ts
+++ Front-end/vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+<<<<<<< Updated upstream
   plugins: [react()],
+=======
+// should be repository name
+base: "",  
+plugins: [react()],
+>>>>>>> Stashed changes
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Set `@` to point to `src`
