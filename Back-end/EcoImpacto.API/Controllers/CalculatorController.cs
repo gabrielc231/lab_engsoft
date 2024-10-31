e8a1bcc6924321798385e01e17d9ab7535af95bf
darkyie <pinguextreme2000@gmail.com>
2024-10-30 21:37:47 -0300

    Feita a conexão com o BD, em localhost.

--- Back-end/EcoImpacto.API/Controllers/CalculatorController.cs
+++ Back-end/EcoImpacto.API/Controllers/CalculatorController.cs
@@ -3,22 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 using EcoImpacto.Application.UseCases.Calculator;
 using EcoImpacto.Communication.Requests;
 using EcoImpacto.Communication.Responses;
+using EcoImpacto.Data;
 
-namespace EcoImpacto.API.Controllers;
-[Route("api/[controller]")]
-[ApiController]
-public class CalculatorController : ControllerBase
+namespace EcoImpacto.API.Controllers
 {
-    [HttpPost]
-    [ProducesResponseType(typeof(ResponseCalculatorResultJson), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public IActionResult Calculator([FromBody] RequestCalculatorResultJson request)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
     {
-        var useCase = new RegisterCalculatorDataUseCase();
+        private readonly EcoImpactoDbContext _dbContext;
 
-        var response = useCase.Execute(request);
+        public CalculatorController(EcoImpactoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-        return Ok(response);
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseCalculatorResultJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Calculator([FromBody] RequestCalculatorResultJson request)
+        {
+            var useCase = new RegisterCalculatorDataUseCase();
+
+            var response = useCase.Execute(request);
+
+            
+            _dbContext.CalculatorData.Add(request);
+            _dbContext.SaveChanges();
+
+            return Ok(response);
+        }
     }
-
 }
--- Back-end/EcoImpacto.Communication/Responses/ResponseCalculatorResultJson.cs
+++ Back-end/EcoImpacto.Communication/Responses/ResponseCalculatorResultJson.cs
@@ -1,6 +1,16 @@
-﻿namespace EcoImpacto.Communication.Responses;
-public class ResponseCalculatorResultJson
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoImpacto.Communication.Responses
 {
-    public double result { get; set; }
-    public string worstSector { get; set; }
+    [Table("EcoImpactoResults")]
+    public class ResponseCalculatorResultJson
+    {
+        [Key]
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public double result { get; set; }
+        public string worstSector { get; set; }
+    }
 }
